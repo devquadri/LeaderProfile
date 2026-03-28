dfe8129c2ac801aaa94cedaf82ff2042b4d42f84
Mohammed Riyazuddin <contact@riyaz.blog>
2026-03-28 12:15:59 +0530

    Fix Tailwind content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "tailwindcss";
 export default {
   darkMode: ["class"],
   content: [
-    "./client/index.html",
-    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
